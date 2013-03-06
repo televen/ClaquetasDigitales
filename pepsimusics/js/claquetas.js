e51c12868945aac07af0089b47a47ea04a8e6359
Victor Acosta <vacosta@televen.vom>
2013-03-06 16:12:31 -0430

    cambios claquetas

---
+++ pepsimusics/js/claquetas.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('.show').click(function(){
+		$('.band').html($('.band_input').val());
+		$('.name').html($('.name_input').val());
+		$('.inputs_wrapper').hide();
+		$('.txt_wrapper').fadeIn();
+	});
+});
