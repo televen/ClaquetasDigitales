8f04d40660d87435533f48c86399b586d2bed7c2
Victor Acosta <vacosta@televen.vom>
2013-03-13 18:37:58 -0430

    websql

--- umpg/js/claquetas.js
+++ umpg/js/claquetas.js
@@ -1,4 +1,42 @@
-var Contestant =  {
+function Contestant(){}
+
+Contestant.prototype.db		 		= '';
+Contestant.prototype.dbname 		= 'televen::contestant';
+Contestant.prototype.dbversion 		= '1.0';
+Contestant.prototype.dbdescription 	= 'Contestants db. Will store all the casting data';
+Contestant.prototype.dbsize		 	= 5 * 1024 * 1024;
+Contestant.prototype.sql		 	= '';
+
+Contestant.prototype.init			= function(){
+	this.db = openDatabase(this.dbname, this.dbversion, this.dbdescription, this.dbsize);
+	if(this.db){
+		this.sql = "CREATE TABLE IF NOT EXISTS " + this.dbname + ".contestant(id INT PRIMARY KEY AUTOINCREMENT, ci INT, first_name TEXT, last_name TEXT, email TEXT, phone TEXT, twitter TEXT, city TEXT, game TEXT, code TEXT, timestamp REAL)";
+		this.db.transaction(
+		function(tx){
+			tx.executeSql(this.sql, [], this.success, this.showError);
+		});
+	}else{
+		console.error("Can't open Database televen::contestant");
+		return false;
+	}
+}
+
+Contestant.prototype.add			= function(entry){
+	
+}
+
+Contestant.prototype.showError		= function(error){
+	console.error("Can't create table Contestant:" + error.message);
+	return false;
+}
+
+Contestant.prototype.success		= function(tx, result){
+	return true;
+}
+
+
+
+/*var Contestant =  {
 	INDEX 	: window.localStorage.getItem("Contestant:INDEX"),
 	SHOW	: $('.show'),
 	
@@ -12,7 +50,7 @@ var Contestant =  {
 		$('input').val("");
 		$('.id-entry').val((window.localStorage.length == 0) ? 0 : window.localStorage.length - 1);
 		// add eventListeners
-		Contestant.SHOW.addEventListener('click', function(e){
+		Contestant.SHOW.on('click', function(e){
 			var entry = {
 				id 			: $('.id-entry').val(),
 				ci 			: $('.ci').val(),
@@ -23,6 +61,7 @@ var Contestant =  {
 				twitter		: $('.twitter').val(),
 				city		: $('.city').val(),
 				game		: $('.game').val(),
+				code		: hex_md5($('.ci').val())
 			};
 			Contestant.add(entry);
 			$('input').val("");
@@ -35,12 +74,13 @@ var Contestant =  {
 		entry.id = Contestant.INDEX;
         window.localStorage.setItem("Contestant:"+ entry.id, JSON.stringify(entry));
         window.localStorage.setItem("Contestant:INDEX", ++Contestant.INDEX);
+		Contestant.showCode(entry.code);
 	},
 	
 	remove 	: function(entry){},
 	
 	list 	: function(){
-		/*if (window.localStorage.length - 1) {
+		if (window.localStorage.length - 1) {
             var contacts_list = [], i, key;
             for (i = 0; i < window.localStorage.length; i++) {
                 key = window.localStorage.key(i);
@@ -56,10 +96,16 @@ var Contestant =  {
                     })
                     .forEach(Contacts.tableAdd);
             }
-        }*/
+        }
+	},
+	
+	showCode: function(code){
+		console.debug(code);
 	}
-}
+};
 
+Contestant.init();*/
+/*
 $(function(){
 	$('.show').click(function(){
 		$('.band').html($('.band_input').val());
@@ -67,6 +113,8 @@ $(function(){
 		$('.inputs_wrapper').hide();
 		$('.txt_wrapper').fadeIn();
 	});
+	
+	console.debug(hex_md5("17402962"));
 });
 
 window.onload = function() {
@@ -75,4 +123,14 @@ window.onload = function() {
 		$('.txt_wrapper').hide();
 		$('.inputs_wrapper').fadeIn();
 	}
-};
+};*/
+
+window.onload = function() {
+	var contestant = new Contestant();
+	contestant.init();
+}/*
+
+$(function(){
+	var contestant = new Contestant();
+	contestant.init();
+});*/
