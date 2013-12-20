08353c84ad0cd20c7094ad7c70894c15df9a1ebd
Victor Acosta <vacosta@televen.vom>
2013-12-20 14:27:02 -0430

    m

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+
+?>
+<html>
+	<head>
+		<title>Claquetas digitales | Admin</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<!-- FRAMEWORK -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../framework/css/bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="CD-wrapper">
+			<div class="step-1">
+				<div class="inner-half">
+					<h1>Bienvenidos al administrador de claquetas</h1>
+					<h4>Te guiaremos en la creación del generador de claquetas para tu evento.</h4>
+					<a href="#" class="btn btn-lg btn-success">¡Empezar! <span class="glyphicon glyphicon-fire"></span></a>
+				</div>
+			</div>
+			<div class="step-2">
+				<div class="inner-half">
+					<h2>¿Qué nombre tendrá el evento?</h2>
+					<div class="input-group">
+						<input type="text" class="form-control">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">Go!</button>
+						</span>
+					</div>
+					
+					<!--<input type="text" id="event-name"/>
+					<a href="#" class="btn btn-large btn-success">Continuar &raquo;</a>-->
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
